a31df52fb1434b6fd6e1368d54026a1b0ba55b2e
skamok <sk_amok@mail.ru>
2021-01-07 19:10:15 +0300

    feat: add firebase functions

--- src/assets/scripts/App.js
+++ src/assets/scripts/App.js
@@ -16,19 +16,82 @@ const formSignUp = {
 formSignUp.EEMail = formSignUp.form.querySelector('#email');
 formSignUp.EPassword = formSignUp.form.querySelector('#password');
 formSignUp.BtnSignUp = formSignUp.form.querySelector('#btnsignup');
+const formSignIn = {
+  form: document.getElementById('formsignin')
+};
+formSignIn.EEMail = formSignIn.form.querySelector('#signinemail');
+formSignIn.EPassword = formSignIn.form.querySelector('#signinpassword');
+formSignIn.BtnSignIn = formSignIn.form.querySelector('#btnsignin');
+formSignIn.BtnSignOut = formSignIn.form.querySelector('#btnsignout');
+
+const formAddLot = {
+  form: document.getElementById('formaddlot')
+};
+formAddLot.EDecsription = formAddLot.form.querySelector('#decsription');
+formAddLot.btnUploadFiles = formAddLot.form.querySelector('#btnuploadfiles');
+formAddLot.ETitle = formAddLot.form.querySelector('#title');
+formAddLot.img0 = formAddLot.form.querySelector('#img0');
+formAddLot.img1 = formAddLot.form.querySelector('#img1');
+formAddLot.btnAddLot = formAddLot.form.querySelector('#btnaddlot');
+
+const formReadLot = {
+  form: document.getElementById('formreadlot')
+};
+formReadLot.EDecsription = formReadLot.form.querySelector('#lotdecsription');
+formReadLot.ETitle = formReadLot.form.querySelector('#lottitle');
+formReadLot.img0 = formReadLot.form.querySelector('#lotimg0');
+formReadLot.img1 = formReadLot.form.querySelector('#lotimg1');
+formReadLot.btnReadLot = formReadLot.form.querySelector('#btnreadlot');
 
 export default class App {
   constructor() {
     this.firebase = new Firebase(firebaseConfig, true);
     this.formSignUp = formSignUp;
+    this.formSignIn = formSignIn;
+    this.formAddLot = formAddLot;
+    this.formReadLot = formReadLot;
   }
 
   init() {
     this.formSignUp.form.addEventListener('submit', this.signUp);
+    this.formSignIn.form.addEventListener('submit', this.signIn);
+    this.formSignIn.form.addEventListener('reset', this.signOut);
+    this.formAddLot.btnAddLot.addEventListener('click', this.addLot);
+    this.formReadLot.btnReadLot.addEventListener('click', this.readLot);
   }
 
   signUp = (event) => {
     event.preventDefault();
     this.firebase.signUP(this.formSignUp.EEMail.value, this.formSignUp.EPassword.value);
+  }
+
+  signIn = (event) => {
+    event.preventDefault();
+    this.firebase.signIN(this.formSignIn.EEMail.value, this.formSignIn.EPassword.value);
+  }
+
+  signOut = (event) => {
+    event.preventDefault();
+    this.firebase.signOUT();
+  }
+
+  addLot = () => {
+    this.formAddLot.img0.src = URL.createObjectURL(this.formAddLot.btnUploadFiles.files[0]);
+    this.formAddLot.img1.src = URL.createObjectURL(this.formAddLot.btnUploadFiles.files[1]);
+    const lot = {
+      title: this.formAddLot.ETitle.value,
+      description: this.formAddLot.EDecsription.value,
+      imgFiles: this.formAddLot.btnUploadFiles.files
+    };
+    this.firebase.addLot(lot);
+  }
+
+  readLot = (event) => {
+    event.preventDefault();
+    this.firebase.readLots()
+      .then((lots) => {
+        const lotsArray = Object.entries(lots);
+        
+      });
   }
 }
--- src/assets/scripts/Firebase.js
+++ src/assets/scripts/Firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/storage';
-import User from './User.js';
+// import User from './User.js';
 
 export default class Firebase {
   constructor(firebaseConfig, debug) {
@@ -10,18 +10,31 @@ export default class Firebase {
     this.fireapp = firebase.initializeApp(firebaseConfig);
     Firebase.log('firebase.initializeApp');
     this.auth = this.fireapp.auth();
+    this.auth.onAuthStateChanged((user) => {
+      if (user) {
+        Firebase.log('firebase.onAuthStateChanged User is signed IN', user.email, user.uid);
+      } else {
+        Firebase.log('firebase.onAuthStateChanged User is signed out');
+      }
+    });
     this.database = this.fireapp.database();
     this.storage = this.fireapp.storage();
     this.storageRef = this.storage.ref();
+    this.storageLotsRef = this.storageRef.child('lots');
     this.usersNode = this.database.ref('users');
+    this.lotsNode = this.database.ref('lots');
   }
 
   signUP(email, password) {
     const retPromice = this.auth.createUserWithEmailAndPassword(email, password)
       .then((userCredential) => {
-        const user = new User(email);
-        Firebase.log('firebase.signUP OK', user, userCredential.user);
-        this.addUser(user);
+        // const user = new User(email);
+        const user = {
+          email
+        };
+        Firebase.log('firebase.signUP OK', userCredential.user.uid);
+        const id = userCredential.user.uid;
+        this.addUser(id, user);
       })
       .catch((error) => {
         const obj = {
@@ -34,11 +47,78 @@ export default class Firebase {
     return retPromice;
   }
 
-  addUser(user) {
-    const newUser = this.usersNode.push();
-    Firebase.log('newUserID =', newUser.key);
-    const retPromice = newUser.set(user);
+  signIN(email, password) {
+    const retPromice = this.auth.signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        Firebase.log('firebase.signIN OK', userCredential.user.email, userCredential.user.uid);
+        return userCredential;
+      })
+      .catch((e) => {
+        const obj = {
+          code: e.code,
+          message: e.message
+        };
+        Firebase.log('firebase.signIN error', obj);
+        return new Error(e.message);
+      });
     return retPromice;
+  }
+
+  signOUT() {
+    this.auth.signOut();
+  }
+
+  addUser(uid, user) {
+    const newUser = this.usersNode.child(uid);
+    Firebase.log('firebase.newUserID =', newUser);
+    const retPromice = newUser.set(user).catch((e) => {
+      const obj = {
+        code: e.code,
+        message: e.message
+      };
+      Firebase.log('firebase.addUser error', obj);
+      return new Error(e.message);
+    });
+    return retPromice;
+  }
+
+  addLot(lot) {
+    const imgsArray = [];
+    const userID = this.auth.currentUser.uid;
+    const lotId = this.lotsNode.push().key;
+    const lotStorageRef = this.storageLotsRef.child(lotId);
+    for (let index = 0; index < lot.imgFiles.length; index++) {
+      const imgRef = lotStorageRef.child(lot.imgFiles[index].name);
+      imgsArray.push(imgRef.fullPath);
+      imgRef.put(lot.imgFiles[index]);
+    }
+    const newLot = {
+      userID,
+      title: lot.title,
+      description: lot.description,
+      dtCreate: (new Date()).toJSON(),
+      imgURLs: imgsArray
+    };
+    Firebase.log('firebase.addLot lot =', lotId, newLot);
+    const lotRef = this.lotsNode.child(lotId);
+    const retPromice = lotRef.set(newLot).catch((e) => {
+      const obj = {
+        code: e.code,
+        message: e.message
+      };
+      Firebase.log('firebase.addLot error', obj);
+      return new Error(e.message);
+    });
+    return retPromice;
+  }
+
+  readLots() {
+    const retPromise = this.lotsNode.once('value').then((snapshot) => {
+      const data = snapshot.val();
+      Firebase.log('firebase.readLots', data);
+      return data;
+    });
+    return retPromise;
   }
 
   static log(val, ...rest) {
