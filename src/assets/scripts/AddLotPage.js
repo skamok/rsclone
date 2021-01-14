cd31906aeebea63734e710a825d0825a69397d32
skamok <sk_amok@mail.ru>
2021-01-14 22:21:13 +0300

    fix: resize picture

--- src/assets/scripts/AddLotPage.js
+++ src/assets/scripts/AddLotPage.js
@@ -135,13 +135,21 @@ export default class AddLotPage {
       this.formLot.karma.after(this.createMessageError('enter a positive number'));
     }
     if (inputError === false) {
-      const imgDataURLs = this.inputPhotos.files;
-      // const imgDataURLs = AddLotPage.resizeImagesForServer(this.inputPhotos.files);
+      // const imgDataURLs = this.inputPhotos.files;
+      this.resizeImagesForServer()
+        .then((dataURLs) => {
+          const imgDataURLs = dataURLs;
+          const lotObj = RealDatabase.createLotObj(this.formLot.nameLot.value, this.formLot.descriptionLot.value,
+            this.formLot.karma.value, this.listCategory.selectedIndex, imgDataURLs, this.firebase.auth.currentUser.uid);
+          console.log(lotObj);
+          //return this.firebase.addLotMultiPicURL(lotObj);
+        })
+        .then(() => alert('lot add'));
+      /*
       const lotObj = RealDatabase.createLotObj(this.formLot.nameLot.value, this.formLot.descriptionLot.value,
         this.formLot.karma.value, this.listCategory.selectedIndex, imgDataURLs, this.firebase.auth.currentUser.uid);
-      // console.log(lotObj);
-      this.firebase.addLotMultiPic(lotObj).then(() => alert('ok'));
-      // this.firebase.addLotMultiPicURL(lotObj).then(() => alert('ok'));
+      */
+      // this.firebase.addLotMultiPic(lotObj).then(() => alert('ok'));
     }
   }
 
@@ -189,69 +197,43 @@ export default class AddLotPage {
     }
   }
 
-  static resizeImagesForServerOld(files) {
-    const arrImages = [];
+  async resizeImagesForServer() {
+    this.arrImages = [];
     const MAX_WIDTH = 800;
     const MAX_HEIGHT = 600;
-    for (let i = 0; i < files.length; i += 1) {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const elem of this.inputPhotos.files) {
       const img = document.createElement('img');
-      img.src = window.URL.createObjectURL(files[i]);
+      img.src = window.URL.createObjectURL(elem);
       const contain = document.createElement('div');
       contain.classList.add('contain');
       const canvas = document.createElement('CANVAS');
       const ctx = canvas.getContext('2d');
-      img.onload = () => {
-        let { width } = img;
-        let { height } = img;
-        if (width > height) {
-          if (width > MAX_WIDTH) {
-            height *= MAX_WIDTH / width;
-            width = MAX_WIDTH;
+      const promise = new Promise((res, rej) => {
+        img.onload = () => {
+          let { width } = img;
+          let { height } = img;
+          if (width > height) {
+            if (width > MAX_WIDTH) {
+              height *= MAX_WIDTH / width;
+              width = MAX_WIDTH;
+            }
+          } else if (height > MAX_HEIGHT) {
+            width *= MAX_HEIGHT / height;
+            height = MAX_HEIGHT;
           }
-        } else if (height > MAX_HEIGHT) {
-          width *= MAX_HEIGHT / height;
-          height = MAX_HEIGHT;
-        }
-        canvas.width = width;
-        canvas.height = height;
-        ctx.drawImage(img, 0, 0, width, height);
-        const dataurl = canvas.toDataURL('image/png', 0.99);
-        arrImages.push(dataurl);
-      };
+          canvas.width = width;
+          canvas.height = height;
+          ctx.drawImage(img, 0, 0, width, height);
+          const dataurl = canvas.toDataURL('image/png', 0.9);
+          res(dataurl);
+          rej(new Error('Photo do not add'));
+        };
+      });
+      // eslint-disable-next-line no-await-in-loop
+      const result = await promise;
+      this.arrImages.push(result);
     }
-    return arrImages;
-  }
-
-  static resizeImagesForServer(files) {
-    const arrImages = [];
-    const MAX_WIDTH = 800;
-    const MAX_HEIGHT = 600;
-    for (let i = 0; i < files.length; i += 1) {
-      const img = document.createElement('img');
-      img.src = window.URL.createObjectURL(files[i]);
-      const contain = document.createElement('div');
-      contain.classList.add('contain');
-      const canvas = document.createElement('CANVAS');
-      const ctx = canvas.getContext('2d');
-      img.onload = () => {
-        let { width } = img;
-        let { height } = img;
-        if (width > height) {
-          if (width > MAX_WIDTH) {
-            height *= MAX_WIDTH / width;
-            width = MAX_WIDTH;
-          }
-        } else if (height > MAX_HEIGHT) {
-          width *= MAX_HEIGHT / height;
-          height = MAX_HEIGHT;
-        }
-        canvas.width = width;
-        canvas.height = height;
-        ctx.drawImage(img, 0, 0, width, height);
-      };
-      const dataurl = canvas.toDataURL('image/png', 0.99);
-      arrImages.push(dataurl);
-    }
-    return arrImages;
+    return this.arrImages;
   }
 }
