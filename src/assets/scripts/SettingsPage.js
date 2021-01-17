16110fe449e4c128fc615b34da7b4698ab7e2ede
tomsonst <40933547+tomsonst@users.noreply.github.com>
2021-01-17 12:50:09 +0300

    init: init settings page

--- src/assets/scripts/SettingsPage.js
+++ src/assets/scripts/SettingsPage.js
@@ -20,8 +20,7 @@ export default class SettingsPage {
     this.formSetting.appendChild(this.headForm);
 
     this.avatar = document.createElement('div');
-    this.avatar.classList.add('avatar');
-    this.avatar.innerText = 'No photo';
+    this.avatar.classList.add('avatar-photo');
     this.formSetting.appendChild(this.avatar);
 
     this.inputPhotos = document.createElement('input');
@@ -35,7 +34,7 @@ export default class SettingsPage {
     this.labelBtnAddPhotos = document.createElement('label');
     this.labelBtnAddPhotos.setAttribute('for', 'file');
     this.labelBtnAddPhotos.classList.add('btn');
-    this.labelBtnAddPhotos.classList.add('btn-tertiary');
+    this.labelBtnAddPhotos.classList.add('btn-tertiary-photo');
     this.labelBtnAddPhotos.classList.add('js-labelFile');
     this.formSetting.appendChild(this.labelBtnAddPhotos);
     this.iconBtnPhotos = document.createElement('i');
@@ -53,6 +52,66 @@ export default class SettingsPage {
 
     this.inputPhotos.addEventListener('change', this.changeAvatar.bind(this));
 
+    this.nickName = document.createElement('span');
+    this.nickName.classList.add('name_lot');
+    this.nickName.innerText = 'Your name';
+    this.formSetting.appendChild(this.nickName);
+
+    this.inputNickName = document.createElement('input');
+    this.inputNickName.setAttribute('type', 'text');
+    this.inputNickName.setAttribute('name', 'nickName');
+    this.inputNickName.setAttribute('class', 'name_lot_input');
+    this.inputNickName.setAttribute('placeholder', 'John');
+    this.formSetting.appendChild(this.inputNickName);
+
+    this.email = document.createElement('span');
+    this.email.classList.add('name_lot');
+    this.email.innerText = 'Your email';
+    this.formSetting.appendChild(this.email);
+
+    this.inputEmail = document.createElement('input');
+    this.inputEmail.setAttribute('type', 'text');
+    this.inputEmail.setAttribute('name', 'email');
+    this.inputEmail.setAttribute('class', 'name_lot_input');
+    this.inputEmail.setAttribute('placeholder', 'john@mail.com');
+    this.formSetting.appendChild(this.inputEmail);
+
+    this.password = document.createElement('span');
+    this.password.classList.add('name_lot');
+    this.password.innerText = 'Your password';
+    this.formSetting.appendChild(this.password);
+
+    this.inputPassword = document.createElement('input');
+    this.inputPassword.setAttribute('type', 'text');
+    this.inputPassword.setAttribute('name', 'password');
+    this.inputPassword.setAttribute('class', 'name_lot_input');
+    this.inputPassword.setAttribute('placeholder', 'password');
+    this.formSetting.appendChild(this.inputPassword);
+
+    this.phone = document.createElement('span');
+    this.phone.classList.add('name_lot');
+    this.phone.innerText = 'Your phone number';
+    this.formSetting.appendChild(this.phone);
+
+    this.inputPhone = document.createElement('input');
+    this.inputPhone.setAttribute('type', 'text');
+    this.inputPhone.setAttribute('name', 'phone');
+    this.inputPhone.setAttribute('class', 'name_lot_input');
+    this.inputPhone.setAttribute('placeholder', '+375 XX XXX-XX-XX');
+    this.formSetting.appendChild(this.inputPhone);
+
+    this.location = document.createElement('span');
+    this.location.classList.add('name_lot');
+    this.location.innerText = 'Your location';
+    this.formSetting.appendChild(this.location);
+
+    this.inputLocation = document.createElement('input');
+    this.inputLocation.setAttribute('type', 'text');
+    this.inputLocation.setAttribute('name', 'location');
+    this.inputLocation.setAttribute('class', 'name_lot_input');
+    this.inputLocation.setAttribute('placeholder', 'Homyel');
+    this.formSetting.appendChild(this.inputLocation);
+
     this.btnSubmit = document.createElement('input');
     this.btnSubmit.setAttribute('type', 'submit');
     this.btnSubmit.setAttribute('value', 'Change settings');
@@ -61,11 +120,51 @@ export default class SettingsPage {
   }
 
   // eslint-disable-next-line class-methods-use-this
-  changeAvatar(e) {
-    if (e.target.files.length !== 0) {
-
-      console.log(this.inputPhotos.file);
-      this.avatar.style.backgroundImage = `url(${this.inputPhotos.file})`;
-    }
-  }
+  changeAvatar() {
+   
+    /* const wrapSelectAvatar = document.createElement('div');
+    wrapSelectAvatar.classList.add('wrap_s_a');
+    this.main.appendChild(wrapSelectAvatar);
+    const selectAvatar = document.createElement('div');
+    selectAvatar.classList.add('select_avatar');
+    wrapSelectAvatar.appendChild(selectAvatar);
+ 
+    // this.avatar.style.backgroundImage = `url()`
+    const img = document.createElement('img');
+    img.src = window.URL.createObjectURL(e.target.files[0]);
+    // console.log(img.src)
+    const canvas = document.createElement('CANVAS');
+    const ctx = canvas.getContext('2d');
+    console.log(img.width, img.height);
+    let MAX_WIDTH;
+    let MAX_HEIGHT;
+    img.onload = () => {
+      if (img.width > img.height) {
+        MAX_WIDTH = 600;
+        MAX_HEIGHT = 340;
+      } if (img.width < img.height) {
+        MAX_WIDTH = 340;
+        MAX_HEIGHT = 600;
+      }
+      let { width } = img;
+      let { height } = img;
+      height *= MAX_WIDTH / width;
+      width = MAX_WIDTH;
+      if (height < MAX_HEIGHT) {
+        width *= MAX_HEIGHT / height;
+        height = MAX_HEIGHT;
+      }
+      canvas.width = width;
+      canvas.height = height;
+      if (height > MAX_HEIGHT) {
+        ctx.drawImage(img, 0, 0, width, height);
+      } else {
+        ctx.drawImage(img, 0, 0, width, height);
+      }
+      // const dataurl = canvas.toDataURL('image/jpeg', 0.9);
+      selectAvatar.appendChild(canvas);
+    };
+    // this.wrapPhotos.appendChild(this.contain);
+    // this.contain.appendChild(canvas); */
+  } 
 }
