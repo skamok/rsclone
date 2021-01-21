235895aac04774bf067aca66306bf5cfa2d417d5
Alexey Cherkas <alexey.cherkas1997@gmail.com>
2021-01-21 12:59:59 +0300

    feat: add separete message block

--- src/assets/scripts/MessagesSection.js
+++ src/assets/scripts/MessagesSection.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 export default class MessagesSection {
   constructor(firebase, parentContainer, main, header) {
     this.firebase = firebase;
@@ -8,9 +9,19 @@ export default class MessagesSection {
 
   createMessagesSection() {
     this.parentContainer.innerHTML = '';
-    this.firebase.readCurrentUser()
+    this.firebase.readCurrentUserChats()
       .then((data) => {
         console.log(data);
+
+        this.messagesContainer = document.createElement('div');
+        this.messagesContainer.classList.add('messages_container', 'animation');
+        this.parentContainer.appendChild(this.messagesContainer);
+
+        for (let i = 0; i < data.length; i++) {
+          this.separateMessageBlock = document.createElement('div');
+          this.separateMessageBlock.classList.add('separate_message');
+          this.messagesContainer.appendChild(this.separateMessageBlock);
+        }
       });
   }
 }
