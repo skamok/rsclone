c0bf0e07826a5aa6f870202165ead3804640a299
tomsonst <40933547+tomsonst@users.noreply.github.com>
2021-01-25 21:17:26 +0300

    feat: add map in settingsPage

---
+++ src/assets/scripts/inputMap.js
@@ -0,0 +1,109 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-undef */
+/* eslint-disable no-use-before-define */
+export default function startMap() {
+  ymaps.ready(init);
+}
+
+function init() {
+  // eslint-disable-next-line no-unused-vars
+  const suggestView = new ymaps.SuggestView('suggest');
+  let map;
+  let placemark;
+  // При клике по кнопке запускаем верификацию введёных данных.
+  const inputMap = document.querySelector('.input_map');
+  // eslint-disable-next-line no-use-before-define
+  // eslint-disable-next-line no-unused-expressions
+  inputMap.addEventListener('keydown', (e) => { e.key === 'Enter' ? geocode() : false; });
+  inputMap.addEventListener('blur', () => { geocode(); });
+
+  function geocode() {
+    // Забираем запрос из поля ввода.
+    const request = document.querySelector('#suggest').value;
+    // Геокодируем введённые данные.
+    ymaps.geocode(request).then((res) => {
+      const obj = res.geoObjects.get(0);
+      let error;
+      let hint;
+
+      if (obj) {
+        switch (obj.properties.get('metaDataProperty.GeocoderMetaData.precision')) {
+          case 'exact':
+            break;
+          case 'number':
+          case 'near':
+          case 'range':
+            error = 'Неточный адрес, требуется уточнение';
+            hint = 'Уточните номер дома';
+            break;
+          case 'street':
+            error = 'Неполный адрес, требуется уточнение';
+            hint = 'Уточните номер дома';
+            break;
+          case 'other':
+          default:
+            error = 'Неточный адрес, требуется уточнение';
+            hint = 'Уточните адрес';
+        }
+      } else {
+        error = 'Адрес не найден';
+        // eslint-disable-next-line no-unused-vars
+        hint = 'Уточните адрес';
+      }
+      if (error) {
+        showError(error);
+      } else {
+        showResult(obj);
+      }
+    }, (e) => {
+      // eslint-disable-next-line no-console
+      console.log(e);
+    });
+  }
+  function showResult(obj) {
+    const shortAddress = obj.properties._data.name;
+    const mapState = {
+      center: obj.geometry._coordinates,
+      controls: [],
+      zoom: 14
+    };
+    createMap(mapState, shortAddress);
+  }
+
+  function showError(message) {
+    const mapContainer = document.querySelector('#map');
+    mapContainer.style.display = 'none';
+    const suggest = document.querySelector('#suggest');
+    suggest.classList.add('input_error');
+    const notice = document.querySelector('#notice');
+    notice.classList.add('message_err');
+    notice.innerText = message;
+    notice.style.display = 'block';
+    // Удаляем карту.
+    if (map) {
+      map.destroy();
+      map = null;
+    }
+  }
+
+  function createMap(state, caption) {
+    const mapContainer = document.querySelector('#map');
+    mapContainer.style.display = 'block';
+    if (!map) {
+      map = new ymaps.Map('map', state);
+      placemark = new ymaps.Placemark(
+        state.center, {
+          iconCaption: caption,
+          balloonContent: caption
+        }, {
+          preset: 'islands#redDotIconWithCaption'
+        }
+      );
+      map.geoObjects.add(placemark);
+    } else {
+      map.setCenter(state.center, state.zoom);
+      placemark.geometry.setCoordinates(state.center);
+      placemark.properties.set({ iconCaption: caption, balloonContent: caption });
+    }
+  }
+}
