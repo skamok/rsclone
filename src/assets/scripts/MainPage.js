15b729282e8e4c9c32123439b2f007ae27000680
skamok <sk_amok@mail.ru>
2021-01-31 20:39:25 +0300

    feat: add online karmacount change

--- src/assets/scripts/MainPage.js
+++ src/assets/scripts/MainPage.js
@@ -14,6 +14,13 @@ export default class MainPage {
     this.firebase = firebase;
   }
 
+  reloadKarmaCount = (dataSnapshot) => {
+    const karmaCount = dataSnapshot.val();
+    if ((karmaCount !== undefined) && (karmaCount !== null)) {
+      this.karmaPoints.innerText = `${karmaCount} karma`;
+    }
+  }
+
   createMainPage() {
     this.firebase.readCurrentUser()
       .then((userData) => {
@@ -64,6 +71,7 @@ export default class MainPage {
         this.karmaPoints.classList.add('karma_header');
         this.karmaPoints.innerText = `${userData.karmaCount} karma`;
         this.profileSubcontainer.appendChild(this.karmaPoints);
+        this.firebase.readCurrentUserKarmaContinues(this.reloadKarmaCount);
 
         this.header.appendChild(this.profileContainer);
 
